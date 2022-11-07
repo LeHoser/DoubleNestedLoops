759ee459701f0b517542210afbe40126890a93b1
LeHoser <nathan.lloyd.7003@gmail.com>
2022-11-07 11:03:46 -0400

    Created Nested Loops

--- DoubleNestedLoops/Program.cs
+++ DoubleNestedLoops/Program.cs
@@ -13,7 +13,18 @@ namespace DoubleNestedLoops
             Console.WriteLine("Double Nested Loops");
             Console.WriteLine();
 
-            
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j += 2)
+                {
+                    for (int k = 0; k <= 9; k += 3)
+                    {
+                        Console.WriteLine(i + "" + j + "" + k);
+                    }
+                }
+
+            }
+
             Console.WriteLine();
             Console.WriteLine("Press and key to exit");
             Console.ReadKey(true);
